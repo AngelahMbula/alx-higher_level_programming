64faee14307880e5ea2641342cffa15629042176
AngelahMbula <mbula.angelah@gmail.com>
2022-08-29 04:30:33 -0700

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    multiples = my_list[:]
+    for count, i in enumerate(my_list):
+        if my_list[i] % 2 == 0:
+            multiples[count] = True
+        else:
+            multiples[count] = False
+    return (multiples)
