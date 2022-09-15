10c3ce821aebf545e18837ded17c3e27a0878f39
AngelahMbula <mbula.angelah@gmail.com>
2022-09-15 11:05:28 -0700

    initial commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """Initializes a new square"""
+    def __init___(self, size):
+        self.size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
