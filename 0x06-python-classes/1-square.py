3abc383b1a6ea49d9977d665bb88fc11830b6618
AngelahMbula <mbula.angelah@gmail.com>
2022-09-15 04:34:48 -0700

    updates

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
+"""define a class square."""
 
 class Square:
     """defines a square with private instance attribute size."""
-    def _init_(self, size):
+    def __init__(self, size):
         self.__size = size
