dd0bca8c606b3d3bd328bc76d20905f377b495a1
AngelahMbula <mbula.angelah@gmail.com>
2022-09-21 02:22:09 -0700

    initial commit

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''module: 5-rectangle
+defines the class Rectangle...
+'''
+
+
+class Rectangle:
+    '''class: Rectangle
+    is an empty class
+    '''
+
+    def __init__(self, width=0, height=0):
+        '''Initializes class Rectangle
+        '''
+        self.width = width
+        self.height = height
