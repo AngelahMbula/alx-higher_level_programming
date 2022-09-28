c7d592ce573885da11bbd053c987d4797a908967
AngelahMbula <mbula.angelah@gmail.com>
2022-09-28 07:53:13 -0700

    initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines BaseGeometry."""
+
+
+class BaseGeometry:
+    """represent BaseGeometry class."""
+    def area(self):
+        """area not implemented."""
+        raise Exception("area() is not implemented")
