da44a2414ed3b729b897717d586cebd3b7363b17
AngelahMbula <mbula.angelah@gmail.com>
2022-11-28 16:30:15 +0000

    update

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,9 +3,9 @@ const array = process.argv.slice(2);
 if (array[0] === undefined && array[1] === undefined) {
   console.log('undefined is undefined');
 } else if (array[0] !== undefined && array[1] !== undefined) {
-  console.log(array[0] + 'is' + array[1]);
+  console.log(array[0] + ' is ' + array[1]);
 } else if (array === undefined && array[1] !== undefined) {
-  console.log('undefined is' + array[1]);
+  console.log('undefined is ' + array[1]);
 } else if (array[0] !== undefined && array[1] === undefined) {
-  console.log(array[0] + 'is undefined');
+  console.log(array[0] + ' is undefined');
 }
