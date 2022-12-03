bdbd3463f83c4be514542f034fd391fda8456da7
AngelahMbula <mbula.angelah@gmail.com>
2022-12-03 05:10:42 -0800

    initial commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script that lists states with name starting with N from database"""
+if __name__== "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+            password=sys.argv[2], database=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY id ASC")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
