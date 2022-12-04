36217ed36ddd6d93ebe26dcb30d21e238574b559
AngelahMbula <mbula.angelah@gmail.com>
2022-12-04 01:16:20 -0800

    updates

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     c = db.cursor()
     c.execute("""SELECT cities.id, cities.name, states.name
     FROM cities
-    LEFT JOIN states ON cities.states_id = states.id
+    INNER JOIN states ON cities.states_id = states.id
     ORDER BY cities.id ASC""")
     [print(city) for city in c.fetchall()]
