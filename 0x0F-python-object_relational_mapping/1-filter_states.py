97404c40192c5c488c12bcde5cd48b87fbb91be2
AngelahMbula <mbula.angelah@gmail.com>
2022-12-04 00:28:52 -0800

    initial commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Script that lists states with name starting with N from database"""
-if __name__== "__main__":
+if __name__ == "__main__":
     import sys
     import MySQLdb
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+    db = MySQLdb.connect(
+            host='localhost', port=3306, user=sys.argv[1],
             password=sys.argv[2], database=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY id ASC")
