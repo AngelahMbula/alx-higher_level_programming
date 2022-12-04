57b47839cd4d7f3f920b6125d01a8b7f62f81e1b
AngelahMbula <mbula.angelah@gmail.com>
2022-12-04 01:13:21 -0800

    initial commit

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Script that lists all cities from the database."""
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(
+            host='localhost', port=3306, user=sys.argv[1],
+            password=sys.argv[2], database=sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT cities.id, cities.name, states.name
+    FROM cities
+    LEFT JOIN states ON cities.states_id = states.id
+    ORDER BY cities.id ASC""")
+    [print(city) for city in c.fetchall()]
