cadd6886ea8c0ab450c173c9212b002f71a5142d
AngelahMbula <mbula.angelah@gmail.com>
2022-12-16 08:55:47 -0800

    initial commit

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""takes in a url, sends a request to the url."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers.get("X-Request-Id"))
